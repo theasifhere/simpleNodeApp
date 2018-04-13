e287975bc344317dc6ec112977d1739ede4c2474
Asif <you@example.com>
2018-04-14 01:37:53 +0500

    Seventh

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000,
 
 server_ip_address   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
