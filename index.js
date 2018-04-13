d906ad6f32ea9d272df26679acd20f58e5b47711
Asif <you@example.com>
2018-04-13 22:44:16 +0500

    Fourth

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http');
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+
+var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+
+server_ip_address   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 http.createServer((req, res) => {
     res.end('Hello Developer');
